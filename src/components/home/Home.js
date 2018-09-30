2184a7a44917e40505467d028e61afc66c4afc73
Dylan <pajorr2@gmail.com>
2018-09-30 19:53:38 +0700

    First Skeleton

---
+++ src/components/home/Home.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import { Avatar } from 'react-native-elements';
+
+class Home extends Component {
+    render() {
+        return (
+            <Avatar
+                small
+                rounded
+                source={{uri: "https://i.gifer.com/MwHJ.gif"}}
+                onPress={() => console.log("Works!")}
+                activeOpacity={0.7}
+            />
+        )
+    }
+}
+export default Home;
